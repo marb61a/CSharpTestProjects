9743f7f24bec8e938fdaefd7652a209afff6974e
Martin <marb61a@gmail.com>
2021-01-26 19:32:03 +0000

    Updated card test

--- Royale.Tests/CardTests.cs
+++ Royale.Tests/CardTests.cs
@@ -1,18 +1,37 @@
+using System.IO;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Royale.Tests
 {
     public class CardTests
     {
+        IWebDriver driver;
+
         [SetUp]
-        public void Setup()
+        public void BeforeEach()
         {
+            driver = new ChromeDriver(Path.GetFullPath(@"../../../../" + "_drivers"));
+        }
+
+        public void AfterEach(){
+
         }
 
         [Test]
         public void Ice_Spirit_is_on_Cards_Page()
         {
-            Assert.Pass();
+            // Go to statsroyale.com site
+            driver.Url = "https://statsroyale.com";
+
+            // Click the cards link in the header
+            driver.FindElement(By.CssSelector("a[href='/cards']")).Click();
+
+            // Assert Ice Spirit is displayed
+            var iceSpirit = driver.FindElement(By.CssSelector("a[href*='Ice+Spirit']"));
+            Assert.That(iceSpirit.Displayed);
+            
         }
     }
 }
