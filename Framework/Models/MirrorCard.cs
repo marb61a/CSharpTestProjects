3e00d70c6f19b602def3192d687e5c812a801d51
Martin <marb61a@gmail.com>
2021-01-27 19:50:32 +0000

    Added framework model files

---
+++ Framework/Models/MirrorCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Framework.Models
+{
+    public class MirrorCard : Card
+    {
+        public string Name { get; set; } = "Mirror";
+
+        public int Cost { get; set; } = 1;
+
+        public string Rarity { get; set; } = "Epic";
+
+        public string Type { get; set; } = "Spell";
+
+        public string Arena { get; set; } = "Arena 12";
+    }
+}
