0537da7bac89a3aeaf479b7fff7f050231577843
Martin <marb61a@gmail.com>
2021-01-28 18:33:00 +0000

    Added Pages Pages.cs

---
+++ Royale/Pages/Pages.cs
@@ -0,0 +1,21 @@
+using System;
+using Framework.Selenium;
+
+namespace Royale.Pages
+{
+    public class Pages
+    {
+        [ThreadStatic]
+        public static CardsPage Cards;
+
+        [ThreadStatic]
+        public static CardDetailsPage CardDetails;
+
+        public static void Init()
+        {
+            Cards = new CardsPage();
+            CardDetails = new CardDetailsPage();
+        }
+        
+    }
+}
