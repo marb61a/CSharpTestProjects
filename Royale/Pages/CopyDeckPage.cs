7866bb2d1280ce5c457f6238e4f97370aff50656
Martin <marb61a@gmail.com>
2021-01-31 19:01:25 +0000

    Updated CopyDeckPage file

--- Royale/Pages/CopyDeckPage.cs
+++ Royale/Pages/CopyDeckPage.cs
@@ -14,13 +14,45 @@ namespace Royale.Pages
 
         public CopyDeckPage Yes()
         {
+            Map.YesButton.Click();
+            Driver.Wait.Until(drvr => Map.CopiedMessage.Displayed);
+            return this;
+        }
 
+        public CopyDeckPage No()
+        {
+            Map.NoButton.Click();
+            AcceptCookies();
+            Driver.Wait.Until(drvr => Map.OtherStoresButton.Displayed);
+            return this;
+        }
+
+        public void AcceptCookies()
+        {
+            Map.AcceptCookiesButton.Click();
+            Driver.Wait.Until(drvr => !Map.AcceptCookiesButton.Displayed);
+        }
+
+        public void OpenAppStore()
+        {
+            Map.AppStoreButton.Click();
+        }
+
+        public void OpenGooglePlay()
+        {
+            Map.GooglePlayButton.Click();
         }
     }
 
     public class CopyDeckPageMap
     {
         public IWebElement YesButton => Driver.FindElement(By.Id("button-open"));
-        
+        public IWebElement NoButton => Driver.FindElement(By.Id("not-installed"));
+        public IWebElement CopiedMessage => Driver.FindElement(By.CssSelector(".notes.active"));
+
+        public IWebElement OtherStoresButton => Driver.FindElement(By.Id("other-stores"));
+        public IWebElement AcceptCookiesButton => Driver.FindElement(By.CssSelector("a.cc-btn.cc-dismiss"));
+        public IWebElement AppStoreButton => Driver.FindElement(By.XPath("//a[text()='App Store']"));
+        public IWebElement GooglePlayButton => Driver.FindElement(By.XPath("//a[text()='Google Play']"));
     }    
 }
