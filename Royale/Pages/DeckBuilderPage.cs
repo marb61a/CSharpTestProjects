5c55d53a034f5703e30a4d417264efd3972378c1
Martin <marb61a@gmail.com>
2021-02-01 11:49:07 +0000

    Updated Pages and DeckBuilder pages

--- Royale/Pages/DeckBuilderPage.cs
+++ Royale/Pages/DeckBuilderPage.cs
@@ -5,12 +5,36 @@ namespace Royale.Pages
 {
     public class DeckBuilderPage : PageBase
     {
+        public readonly DeckBuilderPageMap Map;
 
+        public DeckBuilderPage()
+        {
+            Map = new DeckBuilderPageMap();
+        }
+
+        public DeckBuilderPage GoTo()
+        {
+            HeaderNav.Map.DeckBuilderLink.Click();
+            Driver.Wait.Until(drvr => Map.AddCardsManuallyLink.Displayed);
+            return this;
+        }
+
+        public void AddCardsManually()
+        {
+            Map.AddCardsManuallyLink.Click();
+            Driver.Wait.Until(drvr => Map.CopyDeckIcon.Displayed);
+        }
+
+        public void CopySuggestedDeck()
+        {
+            Map.CopyDeckIcon.Click();
+        }
     }
 
     public class DeckBuilderPageMap
     {
-
+        public IWebElement AddCardsManuallyLink => Driver.FindElement(By.CssSelector(""));
+        public IWebElement CopyDeckIcon => Driver.FindElement(By.XPath("//a[text()='add cards manually']"));
     }
-    
+
 }
