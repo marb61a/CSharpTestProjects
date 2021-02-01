564ba5083a6fbd2f3c09672c79c88e0692ff2e90
Martin <marb61a@gmail.com>
2021-02-01 19:55:55 +0000

    Updated framework logging files

--- Framework/FW.cs
+++ Framework/FW.cs
@@ -9,5 +9,21 @@ namespace Framework
     {
         public static string WORKSPACE_DIRECTORY = Path.GetFullPath(@"../../../../");
         public static Logger Log => _logger ?? throw new NullReferenceException("_logger is null. SetLogger() first.");
+
+        [ThreadStatic]
+        public static DirectoryInfo CurrentTestDirectory;
+
+        [ThreadStatic]
+        private static Logger _logger;
+
+        public static void setLogger()
+        {
+            lock(_setLoggerLock)
+            {
+                
+            }
+        }
+
+        private static object _setLoggerLock = new object();
     }
 }
--- Framework/Logging/Logger.cs
+++ Framework/Logging/Logger.cs
@@ -5,6 +5,22 @@ namespace Framework.Logging
 {
     public class Logger
     {
-        
+        private readonly string _filepath;
+
+        public Logger(string testName, string filepath)
+        {
+            _filepath = filepath;
+
+            using(var log = File.CreateText(_filepath))
+            {
+                log.WriteLine($"Starting Timestamp : {DateTime.Now.ToLocalTime()}");
+                log.WriteLine($"Test: {testName}");
+            }
+        }
+
+        public void Info(string message)
+        {
+            WriteLine($"[INFO]: {message}");
+        }
     }
 }
