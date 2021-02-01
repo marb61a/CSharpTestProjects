0ce903b351ebf3bd189489acc67ad7779b6b06c8
Martin <marb61a@gmail.com>
2021-02-01 20:15:31 +0000

    Updated framework FW file

--- Framework/FW.cs
+++ Framework/FW.cs
@@ -16,11 +16,36 @@ namespace Framework
         [ThreadStatic]
         private static Logger _logger;
 
+        public static DirectoryInfo CreateTestResultsDirectory()
+        {
+            var testDirectory = WORKSPACE_DIRECTORY + "Test Results";
+
+            if(Directory.Exists(testDirectory))
+            {
+                Directory.Delete(testDirectory, recursive: true);
+            }
+
+            return Directory.CreateDirectory(testDirectory);
+        }
+
         public static void setLogger()
         {
             lock(_setLoggerLock)
             {
-                
+                var testResultsDir = WORKSPACE_DIRECTORY + "TestResults";
+                var testName = TestContext.CurrentContext.Test.Name;
+                var fullPath = $"{testResultsDir}/{testName}";
+
+                if(Directory.Exists(fullPath))
+                {
+                    CurrentTestDirectory = Directory.CreateDirectory(fullPath + TestContext.CurrentContext.Test.ID);
+                }
+                else
+                {
+                    CurrentTestDirectory = Directory.CreateDirectory(fullPath);
+                }
+
+                _logger = new Logger(testName, CurrentTestDirectory.FullName + "/log.txt");
             }
         }
 
