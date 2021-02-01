009b30d488f2f004db141999227fb806dffe9923
Martin <marb61a@gmail.com>
2021-02-01 11:21:53 +0000

    Updated Drive and Notes files

--- Framework/Selenium/Driver.cs
+++ Framework/Selenium/Driver.cs
@@ -12,12 +12,18 @@ namespace Framework.Selenium
         [ThreadStatic]
         public static IWebDriver _driver;
 
+        [ThreadStatic]
+        public static Wait Wait;
+
         public static void Init()
         {
             _driver = new ChromeDriver(Path.GetFullPath(@"../../../../" + "_drivers"));
+            Wait = new Wait(10);
         }
 
         public static IWebDriver Current => _driver ?? throw new NullReferenceException("_driver is null");
+
+        public static string Title => Current.Title;
 
         public static void GoTo(string url)
         {
