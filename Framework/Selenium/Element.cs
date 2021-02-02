a8761b9e8fc8005c3c8d24e1e754f8976e366374
Martin <marb61a@gmail.com>
2021-02-02 11:56:00 +0000

    Updated Element file

--- Framework/Selenium/Element.cs
+++ Framework/Selenium/Element.cs
@@ -11,9 +11,37 @@ namespace Framework.Selenium
         public readonly string Name;
         public By FoundBy { get; set; }
 
-        public Element()
+        public Element(IWebElement element, string name)
         {
-            
+            _element = element;
+            Name = name;
+        }
+
+        public IWebElement Current => _element ?? throw new System.NullReferenceException("_element is null.");
+        public string TagName => Current.TagName;
+        public string Text => Current.Text;
+
+        public bool Enabled => Current.Enabled;
+        public bool Selected => Current.Selected;
+        public bool Displayed => Current.Displayed;
+
+        public Point Location => Current.Location;
+        public Size Size => Current.Size;
+
+        public void Clear()
+        {
+            Current.Clear();
+        }
+
+        public void Click()
+        {
+            FW.Log.Step($"Click {Name}");
+            Current.Click();
+        }
+
+        public void Submit()
+        {
+            Current.Submit();
         }
     }
 }
