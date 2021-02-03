3c9215a698d1a0e63850a1658bc34b5224d01434
Martin <marb61a@gmail.com>
2021-02-03 19:38:39 +0000

    Added Framework Selenium WaitCondition file

---
+++ Framework/Selenium/WaitConditions.cs
@@ -0,0 +1,40 @@
+using System;
+using OpenQA.Selenium;
+
+namespace Framework.Selenium
+{
+    public sealed class WaitConditions
+    {
+        public static Func<IWebDriver, bool> ElementDisplayed(IWebElement element)
+        {
+            bool condition(IWebDriver driver)
+            {
+                return element.Displayed;
+            }
+
+            return condition;
+        }
+
+        public static Func<IWebDriver, IWebElement> ElementIsDisplayed(IWebElement element)
+        {
+            IWebElement condition(IWebDriver driver)
+            {
+                try
+                {
+                    return element.Displayed ? element : null;
+                }
+                catch (NoSuchElementException)
+                {
+                    return null;
+                }
+                catch (ElementNotVisibleException)
+                {
+                    return null;
+                }
+                
+            }
+
+            return condition;
+        }
+    }
+}
