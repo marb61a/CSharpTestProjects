3bdf6b67ffa07301fdef33e33985c97555124559
Martin <marb61a@gmail.com>
2021-02-03 11:14:21 +0000

    Updated notes file, added test base class and updated epenencies

---
+++ Royale.Tests/Base/TestBase.cs
@@ -0,0 +1,13 @@
+using Framework;
+using Framework.Selenium;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using Royale.Pages;
+
+namespace Tests.Base
+{
+    public abstract class TestBase
+    {
+        
+    }
+}
